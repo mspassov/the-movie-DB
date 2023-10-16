827884f3cfae33a847d14ec0de75feca6706aa93
Martin Spassov <mspassov@uwo.ca>
2023-10-15 20:15:02 -0400

    Fetch TV shows completed

--- js/script.js
+++ js/script.js
@@ -7,18 +7,30 @@ const fetchAPIData = async (endpoint) =>{
     const API_KEY = 'f3ab67acf3923abec3aca78fa84b47c8';
     const API_URL = 'https://api.themoviedb.org/3/';
 
+    showSpinner();
     const res = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
     const data = await res.json();
+    hideSpinner();
 
     return data;
+}
+
+const showSpinner = () =>{
+    const spinner = document.querySelector('.spinner');
+    spinner.classList.add('show');
+}
+
+const hideSpinner = () =>{
+    const spinner = document.querySelector('.spinner');
+    spinner.classList.remove('show');
 }
 
 //Display the popular movies on the homepage
 const displayPopularMovies = async () =>{
     const { results: popMovies } = await fetchAPIData('movie/popular');
     const movieGrid = document.querySelector('#popular-movies');
-    console.log(popMovies[0]);
-    //Create new movie card
+    
+    //Create new movie cards
     popMovies.forEach((movie) =>{
         const div = document.createElement('div');
         div.classList.add('card');
@@ -37,6 +49,34 @@ const displayPopularMovies = async () =>{
                 </p>
             </div>`
         movieGrid.appendChild(div);
+    })
+
+}
+
+const displayTVShows = async () =>{
+    const { results: popShows } = await fetchAPIData('tv/popular');
+    const showGrid = document.querySelector('#popular-shows');
+    console.log(popShows[0])
+    //Create new TV Shows cards
+    popShows.forEach((show) =>{
+        const div = document.createElement('div');
+        div.innerHTML = 
+        `<div class="card">
+          <a href="tv-details.html?id=${show.id}">
+            <img
+              src="${show.poster_path ? `https://image.tmdb.org/t/p/w500${show.poster_path}` : '../images/no-image.jpg'}"
+              class="card-img-top"
+              alt="${show.name}"
+            />
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Aired: ${show.first_air_date}</small>
+            </p>
+          </div>
+        </div>`
+        showGrid.appendChild(div);
     })
 
 }
@@ -65,7 +105,7 @@ const init = () =>{
             console.log('Search Page');
             break;
         case '/shows.html':
-            console.log('Shows');
+            displayTVShows();
             break;
         case '/tv-details.html':
             console.log('TV Details');
